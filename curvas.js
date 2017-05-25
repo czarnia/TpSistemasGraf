7ce72fe42f6111c053a2d2add5923ac44a4e80a1
sofiamorseletto <sofimorseletto@hotmail.com>
2017-05-24 21:47:35 -0300

    Agrego funciones para calcular los puntos de la curva

--- curvas.js
+++ curvas.js
@@ -41,8 +41,11 @@ function curvaBesier(){
 
 
 //Curva Bspline base 3
-function curvaBspline(){
+function curvaBspline3(){
   this.puntosDeControl = null;
+  this.valores_u = null;
+  this.binormal = vec3.fromValues(0.0,0.0,1.0);
+  this.grilla = null;
 
   //Bases
   this.base0 = function(u) {
@@ -72,10 +75,76 @@ function curvaBspline(){
     return (3*u*u)*1/6;
   }
 
-  this.create = function(puntos, paso) {
+  this.create = function(puntos) {
     this.puntosDeControl = puntos;
     this.paso = paso;
-  }
+    this.valores_u = puntos.length - 3;
+/*    var puntos_curva = [];
+    var indice = 0;
+    if(tf % paso != 0)
+        console.log("El tf debe ser divisible por el paso");*/
 
+    //Conviene devolver el resultado de la funcion en un t que le pasen que devolver todos los puntos
 
+    /*for(var j = 0; j < this.puntosDeControl.length; ++j){
+        for(var i = 0; i <= (tf / paso); ++i){
+            puntos_curva[indice + i] = this.puntosDeControl[j] * this.base0(to + i * paso) 
+                    + this.puntosDeControl[j + 1] * this.base1(to + i * paso) 
+                    + this.puntosDeControl[j + 2] * this.base2(to + i * paso) 
+                    + this.puntosDeControl[j + 3] * this.base3(to + i * paso);
+        }
+        indice += i;*/
+    }
+    //Podria agarrar y devolver los puntos
+    //Lo mismo con la tangente, la normal y la binormal de la curva
+  
+
+    this.get_punto = function(u){
+        //Modifico para que el punto este de 0 a 1
+        var aux = Math.floor(u);
+        var t_local = u - aux;
+        //Los puntos de control van a ser los que esten cerca de t creo
+        return this.puntosDeControl[aux] * this.base0(t_local) 
+                + this.puntosDeControl[aux + 1] * this.base1(t_local) 
+                + this.puntosDeControl[aux + 2] * this.base2(t_local) 
+                + this.puntosDeControl[aux + 3] * this.base3(t_local);
+        //DUDA: ver si se puede calcular asi
+    }
+
+    this.get_tan = function(u){
+        //Modifico para que el punto este de 0 a 1
+        var aux = Math.floor(u);
+        var t_local = u - aux;
+        //Los puntos de control van a ser los que esten cerca de t creo
+        return this.puntosDeControl[aux] * this.base0der(t_local) 
+                + this.puntosDeControl[aux + 1] * this.base1der(t_local) 
+                + this.puntosDeControl[aux + 2] * this.base2der(t_local) 
+                + this.puntosDeControl[aux + 3] * this.base3der(t_local);
+    }
+
+    this.get_normal = function(u){
+        vec3 normal = vec3.create();
+        vec3.cross(normal, this.binormal, this.get_tan(u));
+        return normal;
+    }
+
+    //No muy practico usar grilla para dibujar la curva asi q ver esto
+    this.setupWebGLBuffers = function(){
+        //Hacemos que la grilla dibuje la curva
+        this.grilla = new VertexGrid();
+        this.grilla.create(this.valores_u.length, 1);
+        this.grilla.createIndexBuffer();
+        for (var t = 0; t < this.valores_u.length; t++) {
+            var punto = this.get_punto(t);
+            this.grilla.position_buffer.push(punto[0]);
+            this.grilla.position_buffer.push(punto[1]);
+            this.grilla.position_buffer.push(punto[2]);
+        }
+        this.grilla.setupWebGLBuffers();
+    }
+
+    this.draw = function(){
+        this.grilla.drawVertexGrid();
+    }
 }
+ 
