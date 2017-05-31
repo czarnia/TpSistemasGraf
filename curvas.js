556e78b9222b9190cd2b0a971a35551d6dc8e490
Ana Czarnitzki <czarniana@gmail.com>
2017-05-31 14:44:44 -0300

    Copy-paste de Bessies en Bspline

--- curvas.js
+++ curvas.js
@@ -32,10 +32,115 @@ function curvaBesier(){
     }
 
 
-    this.create = function(puntos, paso) {
+    this.create = function(puntos) {
       this.puntosDeControl = puntos;
-      this.paso = paso;
-    }
+      this.valores_u = puntos.length - 3;
+      }
+
+      this.length = function(){
+          return this.valores_u;
+      }
+
+      this.get_punto = function(u){
+          var vector_aux = vec3.fromValues(0.0, 0.0, 0.0);
+          //Modifico para que el punto este de 0 a 1
+          var aux = Math.floor(u);
+          var u_local = u - aux;
+
+          if (this.valores_u <= u){
+              aux = this.valores_u - 1;
+              u_local = 1;
+          }
+
+          vec3.scaleAndAdd(vector_aux, vector_aux, this.puntosDeControl[aux], this.base0(u_local));
+          vec3.scaleAndAdd(vector_aux, vector_aux, this.puntosDeControl[aux + 1], this.base1(u_local));
+          vec3.scaleAndAdd(vector_aux, vector_aux, this.puntosDeControl[aux + 2], this.base2(u_local));
+          vec3.scaleAndAdd(vector_aux, vector_aux, this.puntosDeControl[aux + 3], this.base3(u_local));
+
+          return vector_aux;
+      }
+
+      this.get_tan = function(u){
+          var vector_aux = vec3.fromValues(0.0, 0.0, 0.0);
+          //Modifico para que el punto este de 0 a 1
+          var aux = Math.floor(u);
+          var u_local = u - aux;
+
+          if (this.valores_u <= u){
+              aux = this.valores_u - 1;
+              u_local = 1;
+          }
+
+          var ok = true;
+          for (var i = 1; i < 4; i++) {
+              for (var j = 0; j < 3; j++){
+                  if (this.puntosDeControl[aux][j] != this.puntosDeControl[aux + i][j])
+                      ok = false;
+              }
+          }
+
+          if (ok == true){
+              if ((aux + 4) < this.puntosDeControl.length){
+                  vec3.subtract(vector_aux, this.puntosDeControl[aux + 4], this.puntosDeControl[aux]);
+                  return vector_aux;
+              }
+              vec3.subtract(vector_aux, this.puntosDeControl[aux], this.puntosDeControl[aux - 1]);
+              return vector_aux;
+          }
+
+          vec3.scaleAndAdd(vector_aux, vector_aux, this.puntosDeControl[aux], this.base0der(u_local));
+          vec3.scaleAndAdd(vector_aux, vector_aux, this.puntosDeControl[aux + 1], this.base1der(u_local));
+          vec3.scaleAndAdd(vector_aux, vector_aux, this.puntosDeControl[aux + 2], this.base2der(u_local));
+          vec3.scaleAndAdd(vector_aux, vector_aux, this.puntosDeControl[aux + 3], this.base3der(u_local));
+
+          return vector_aux;
+      }
+
+      this.get_normal = function(u){
+          var normal = vec3.create();
+          vec3.cross(normal, this.binormal, this.get_tan(u));
+          return normal;
+      }
+
+      this.setupWebGLBuffers = function(){
+          // 1. Creamos un buffer para las posicioens dentro del pipeline.
+          this.webgl_position_buffer = gl.createBuffer();
+          // 2. Le decimos a WebGL que las siguientes operaciones que vamos a ser se aplican sobre el buffer que
+          // hemos creado.
+          gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_position_buffer);
+          // 3. Cargamos datos de las posiciones en el buffer.
+          gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.position_buffer), gl.STATIC_DRAW);
+
+          // Repetimos los pasos 1. 2. y 3. para la informaci�n del color
+          this.webgl_color_buffer = gl.createBuffer();
+          gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_color_buffer);
+          gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.color_buffer), gl.STATIC_DRAW);
+
+          // Repetimos los pasos 1. 2. y 3. para la informaci�n de los �ndices
+          // Notar que esta vez se usa ELEMENT_ARRAY_BUFFER en lugar de ARRAY_BUFFER.
+          // Notar tambi�n que se usa un array de enteros en lugar de floats.
+          this.webgl_index_buffer = gl.createBuffer();
+          gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.webgl_index_buffer);
+          gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.index_buffer), gl.STATIC_DRAW);
+      }
+
+      this.draw = function(){
+
+          var vertexPositionAttribute = gl.getAttribLocation(glProgram, "aVertexPosition");
+          gl.enableVertexAttribArray(vertexPositionAttribute);
+          gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_position_buffer);
+          gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+
+          var vertexColorAttribute = gl.getAttribLocation(glProgram, "aVertexColor");
+          gl.enableVertexAttribArray(vertexColorAttribute);
+          gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_color_buffer);
+          gl.vertexAttribPointer(vertexColorAttribute, 3, gl.FLOAT, false, 0, 0);
+
+          gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.webgl_index_buffer);
+
+          // Dibujamos.
+          gl.drawElements(gl.LINE_STRIP, this.index_buffer.length, gl.UNSIGNED_SHORT, 0);
+      }
 
 }
 
@@ -91,7 +196,7 @@ function curvaBspline3(){
         return this.valores_u;
     }
 
-    this.get_punto = function(u){        
+    this.get_punto = function(u){
         var vector_aux = vec3.fromValues(0.0, 0.0, 0.0);
         //Modifico para que el punto este de 0 a 1
         var aux = Math.floor(u);
@@ -131,10 +236,10 @@ function curvaBspline3(){
 
         if (ok == true){
             if ((aux + 4) < this.puntosDeControl.length){
-                vec3.subtract(vector_aux, this.puntosDeControl[aux + 4], this.puntosDeControl[aux]); 
+                vec3.subtract(vector_aux, this.puntosDeControl[aux + 4], this.puntosDeControl[aux]);
                 return vector_aux;
             }
-            vec3.subtract(vector_aux, this.puntosDeControl[aux], this.puntosDeControl[aux - 1]); 
+            vec3.subtract(vector_aux, this.puntosDeControl[aux], this.puntosDeControl[aux - 1]);
             return vector_aux;
         }
 
@@ -196,7 +301,7 @@ function curvaBspline3(){
     }
 
     this.draw = function(){
-        
+
         var vertexPositionAttribute = gl.getAttribLocation(glProgram, "aVertexPosition");
         gl.enableVertexAttribArray(vertexPositionAttribute);
         gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_position_buffer);
@@ -213,4 +318,3 @@ function curvaBspline3(){
         gl.drawElements(gl.LINE_STRIP, this.index_buffer.length, gl.UNSIGNED_SHORT, 0);
     }
 }
- 
