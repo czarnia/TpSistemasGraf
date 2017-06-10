43abf8b83c3368b5169e6caf443cd8181a31f739
Ana Czarnitzki <czarniana@gmail.com>
2017-06-10 12:36:04 -0300

    Agrego el auto, le agrego color a la rueda y a la superficie de revolución, agrego translate y rotate en la rueda

--- rueda.js
+++ rueda.js
@@ -11,21 +11,29 @@ function Rueda(){
 		this.perfil.forma = [];
 		this.perfil.normal = [];
 
-		this.perfil.forma.push([0.0, 0.0, this.radio]);
-    this.perfil.forma.push([0.0, 0.0, 0.0]);
-    this.perfil.forma.push([0.0, this.profundidad, 0.0]);
-		this.perfil.forma.push([0.0, this.profundidad, this.radio]);
-    this.perfil.forma.push([0.0, 0.0, this.radio]);
+		this.perfil.forma.push([this.radio, 0.0, -this.profundidad/2]);
+    this.perfil.forma.push([0.0, 0.0, -this.profundidad/2]);
+    this.perfil.forma.push([0.0, 0.0, this.profundidad/2]);
+		this.perfil.forma.push([this.radio, 0.0, this.profundidad/2]);
+    this.perfil.forma.push([this.radio, 0.0, -this.profundidad/2]);
 	}
 
-	this.create = function(radio, profundidad){
+	this.create = function(radio, profundidad, color){
     this.radio = radio;
     this.profundidad = profundidad;
 		this.crear_perfil();
-		this.superficie.create([0,1,0], this.perfil.forma, Math.PI*2, 40.0);
+		this.superficie.create([0,0,1], this.perfil.forma, Math.PI*2, 40.0, color);
 	}
 
 	this.draw = function(){
 		this.superficie.draw();
+	}
+
+	this.translate = function(v){
+		this.superficie.translate(v);
+	}
+
+	this.rotate = function(grados, eje){
+		this.superficie.rotate(grados, eje);
 	}
 }
