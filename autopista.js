cede3033889ffe124571e6e8964ff36ce5a08ae9
sofiamorseletto <sofimorseletto@hotmail.com>
2017-06-10 18:31:06 -0300

    Le agrego las luminarias

--- autopista.js
+++ autopista.js
@@ -4,8 +4,9 @@ function Autopista(){
 	this.calle_vuelta = new Calle();
 	this.borde_vuelta = new Borde();
 	this.pilares = [];
+	this.faroles = [];
 
-	this.create = function(curva_camino, dist_pilares, cant_pilares){
+	this.create = function(curva_camino, dist_pilares, dist_faroles){
 		var aux = vec3.create();
 		var sube = -9.75;
 
@@ -31,17 +32,18 @@ function Autopista(){
 
 		curva_camino.discretizar();
 
-		var dist = 0;
-		var i = 0, j = 0;
+		var dist_p = 0, dist_f = 0;
+		var i = 0, signo = 1;
 
-		while((i < curva_camino.discretizaciones.length) && (j < cant_pilares)){
-			if(curva_camino.distancias_discret[i] >= dist){	
-				if(curva_camino.distancias_discret[i] > dist){
+		while((i < curva_camino.discretizaciones.length)){
+			//AGREGO PILARES
+			if(curva_camino.distancias_discret[i] >= dist_p){	
+				if(curva_camino.distancias_discret[i] > dist_p){
 					var u = (curva_camino.discretizaciones[i - 1] + curva_camino.discretizaciones[i]) / 2;
 					var punto = curva_camino.get_punto(u);
 				}
 
-				if(curva_camino.distancias_discret[i] == dist){
+				if(curva_camino.distancias_discret[i] == dist_p){
 					var punto = curva_camino.get_punto(curva_camino.discretizaciones[i]);
 				}
 
@@ -51,8 +53,43 @@ function Autopista(){
 				pilar.rotate(-Math.PI/2, [1.0, 0.0, 0.0]);
 				pilar.translate(punto);
 				this.pilares.push(pilar);
-				j++;
-				dist += dist_pilares;
+				dist_p += dist_pilares;
+			}
+			//AGREGO FAROLES
+			if(curva_camino.distancias_discret[i] >= dist_f){	
+				if(curva_camino.distancias_discret[i] > dist_f){
+					var u = (curva_camino.discretizaciones[i - 1] + curva_camino.discretizaciones[i]) / 2;
+					var punto = curva_camino.get_punto(u);
+					var tan = curva_camino.get_tan(u);
+				}
+
+				if(curva_camino.distancias_discret[i] == dist_f){
+					var punto = curva_camino.get_punto(curva_camino.discretizaciones[i]);
+					var tan = curva_camino.get_tan(curva_camino.discretizaciones[i]);
+				}
+
+				tan_l = [1.0, 0.0, 0.0];
+				var farol = new Luminaria();
+				farol.create(1.0, 20.0, 10.0, 5.0, 4.0, 3.0);
+				farol.setupWebGLBuffers();
+				farol.scale(0.5, 0.5, 0.5);
+				farol.rotate(-Math.PI/2, [1.0, 0.0, 0.0]);
+				farol.rotate(-Math.PI/2, [0.0, 0.0, signo * 1.0]);
+
+				vec3.normalize(tan, tan);
+				vec3.normalize(tan_l, tan_l);
+				var angulo = Math.acos(vec3.dot(tan, tan_l));
+
+				var eje = vec3.create();
+				vec3.cross(eje, tan, tan_l);
+				farol.rotate(-angulo, eje);
+
+				// farol.translate([0.0, 0.0, sube + -1]);	
+				vec3.add(punto, punto, [0.0, 0.0, sube + -1]);
+				farol.translate(punto);				
+				this.faroles.push(farol);
+				dist_f += dist_faroles;
+				signo *= (-1); //Una luminaria para cada lado
 			}
 			i++;
 		}
@@ -154,6 +191,9 @@ function Autopista(){
 		this.borde_vuelta.draw();
 		for (var i = 0; i < this.pilares.length; i++) {
 			this.pilares[i].draw();
+		}
+		for (var i = 0; i < this.faroles.length; i++) {
+			this.faroles[i].draw();
 		}
 	}
 }
